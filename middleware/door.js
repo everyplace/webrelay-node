df6b7b6ed2eaa0a58079a22d2887de28694deb4a
Erin Sparling <erin@everyplace.net>
2013-03-14 16:31:15 -0400

    Swapped out request call to be a get, not a post.

--- middleware/door.js
+++ middleware/door.js
@@ -14,7 +14,7 @@ exports.knock = function(req, res, next){
 exports.open = function(req, res){
 
 	var request_url = (process.env.DEBUG=='true') ? process.env.DEBUG_ACTION_URL : process.env.ACTION_URL;
-	request.post(request_url, function(err, response, body){
+	request.get(request_url, function(err, response, body){
 		console.log("Action triggered");
 		res.end();	
 	});
